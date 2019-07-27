7c6eb9e7e087cd5fb66566cccc200f4cb67f9f3b
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-07-27 19:23:34 -0300

    Exercicio 3

---
+++ ExerciciosPython/ex003.py
@@ -0,0 +1,5 @@
+valorUm = int(input('Digite um valor:'))
+valorDois = int(input('Digite outro valor:'))
+soma = valorUm + valorDois
+
+print('A soma de {} e {} é igual a {}!'.format(valorUm, valorDois, soma))
