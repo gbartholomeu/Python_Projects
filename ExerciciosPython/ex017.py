2ccc37a878d5e563dde5f74a7e24d2d7f9fcf463
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-07-30 01:32:03 -0300

    Exercicio 17 - Corrigido

--- ExerciciosPython/ex017.py
+++ ExerciciosPython/ex017.py
@@ -1,7 +1,12 @@
-import math
+from math import sqrt, pow
 
 catetoOposto = float(input('Digite o tamanho do cateto oposto: '))
 catetoAdjacente = float(input('Digite o tamanho do cateto adjacente: '))
-hipotenusa = math.sqrt(math.pow(catetoOposto, 2) + math.pow(catetoAdjacente, 2))
+hipotenusa = sqrt(pow(catetoOposto, 2) + pow(catetoAdjacente, 2))
+msg = 'O valor da hipotenusa de um triângulo retângulo com um cateto oposto de {} e cateto adjacente de {} é {}'
 
-print('O valor da hipotenusa de um triângulo retângulo com um cateto oposto de {} e cateto adjacente de {} é {}'.format(catetoOposto, catetoAdjacente, hipotenusa))
+print(msg.format(catetoOposto, catetoAdjacente, hipotenusa))
+
+'''resposta da aula: 
+math.hipot(co, ca)
+'''
