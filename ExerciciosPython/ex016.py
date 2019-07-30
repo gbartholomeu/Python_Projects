fae8d5629382d0d7c576c2a901f8bfe0b6987780
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-07-30 01:27:32 -0300

    Exercicio 16 -Corrigido

--- ExerciciosPython/ex016.py
+++ ExerciciosPython/ex016.py
@@ -1,4 +1,7 @@
-import math
+from math import floor, trunc
 
 num = float(input('Digite um número Real por favor: '))
-print('O número {} tem a parte inteira {}'.format(num, math.floor(num)))
+print('O número {} tem a parte inteira {}'.format(num, floor(num)))
+
+#resposta 1 da aula: trunc(num)
+#resposta 2 da aula: int(num)
