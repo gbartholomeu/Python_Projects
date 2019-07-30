e2915305d75dd0aa789c4b85702847a8c06f8102
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-07-30 01:38:19 -0300

    Exercicio 19 - Corrigido

--- ExerciciosPython/ex019.py
+++ ExerciciosPython/ex019.py
@@ -7,3 +7,5 @@ alunoQuatro = str(input('Digite o nome do quarto aluno: '))
 alunoEscolhido = random.choice([alunoUm, alunoDois, alunoTres, alunoQuatro])
 
 print('O aluno escolhido para ajudar o professor foi o/a {}'.format(alunoEscolhido))
+
+'''Resposta corrigida: ele apenas fez o import do choice direto e declarou a lista fora do método'''
