26b747540183217e43f8f0f81c7cce616a599af9
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-07-30 01:34:42 -0300

    Exercicio 18 - Corrigido

--- ExerciciosPython/ex018.py
+++ ExerciciosPython/ex018.py
@@ -1,7 +1,7 @@
 import math
 
 angulo = float(input('Digite um um ângulo: '))
-print('Para o ângulo {}, o valor de seno é {}, de cosseno é {} e o valor da tangente é {}'.format(angulo,
-                                                                                                  math.sin(angulo),
-                                                                                                  math.cos(angulo),
-                                                                                                  math.tan(angulo)))
+print('Para o ângulo {}, o valor de seno é {:.2f}, de cosseno é {:.2f} e o valor da tangente é {:.2f}'.format(angulo,
+                                                                                                  math.sin(math.radians(angulo)),
+                                                                                                  math.cos(math.radians(angulo)),
+                                                                                                  math.tan(math.radians(angulo))))
